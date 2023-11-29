2cbcd8a45fc6890cb388d891ddb6718fb4522c97
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2023-11-29 16:30:29 -0300

    adicionar composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alexandrexandecar/simple-routes',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b0b1ad4931a27dc81ddbd51d2248463a28b4bd33',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexandrexandecar/simple-routes' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b0b1ad4931a27dc81ddbd51d2248463a28b4bd33',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'league/plates' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'reference' => 'a6a3238e46c6e19af7318fdc36bfbe49b0620231',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
