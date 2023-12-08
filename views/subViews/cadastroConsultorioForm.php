c31d3f10c53ad0caabb6ad2bbd1c753a684b499d
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2023-12-08 11:30:06 -0300

    criei o ajax e as consultas

--- views/subViews/cadastroConsultorioForm.php
+++ views/subViews/cadastroConsultorioForm.php
@@ -9,15 +9,17 @@
             <p>Nome da Farmácia</p>
             <p>CNPJ</p>
         </div>
-        <form action="" class="formServicoss">
+        <form action="../../controllers//controllerConsultorio.php" method="POST"  class="formServicoss" id="formConsultorio">
             <div class="infoFlex-Consultorio">
+                <input type="hidden" name="idFarmacia" value="<?php echo $_SESSION['user_id'] ?>">
+                <input type="hidden" name="horaConsulta" value="<?php echo date("H:i:s"); ?>">
                 <div class="lado40px">
                     <label for="NameFarm">Nome do Farmacêutico:</label>
                     <input type="text" name="NameFarm" id="NameFarm">
                 </div>
                 <div>
                     <label for="date">Data</label>
-                    <input type="date" name="date" id="date">
+                    <input type="date" name="date" id="date" autocomplete>
                 </div>
             </div>
 
@@ -42,3 +44,6 @@
         </form>
     </div>
 </section>
+<div id="resultado">
+
+</div>
